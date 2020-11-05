b5b7aae3b7a4b3411348280c2b636312ce769382
Junior Alves <jjalves96@gmail.com>
2020-11-05 16:37:04 -0300

    chore: add new components into Layout

--- src/components/Layout/styled.ts
+++ src/components/Layout/styled.ts
@@ -3,6 +3,6 @@ import styled from 'styled-components/native';
 export const Layout = styled.View`
   width: 100%;
   flex: 1;
-  padding: 20px 20px;
+  padding: 12px 20px;
   align-items: center;
 `;
