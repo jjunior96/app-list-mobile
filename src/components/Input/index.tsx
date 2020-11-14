8eb67a33e43403ef8fde20d92737687f3f200668
Junior Alves <jjalves96@gmail.com>
2020-11-14 07:40:02 -0300

    chore: add unform lib in component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {TextInputProps} from 'react-native';
+import {useField} from '@unform/core';
 
 import * as S from './styled';
 
@@ -8,12 +9,29 @@ interface InputProps extends TextInputProps {
   icon?: string;
 }
 
+interface InpuValueRef {
+  value: string;
+}
+
 const Input: React.FC<InputProps> = ({name, icon, ...rest}) => {
+  const {registerField, defaultValue, fieldName, error} = useField(name);
+  const inputValueRef = useRef<InpuValueRef>({value: defaultValue});
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputValueRef.current.value,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
   return (
     <S.Container>
       <S.TextInput
         keyboardAppearance="dark"
         placeholderTextColor="#62707F"
+        defaultValue={defaultValue}
+        onChangeText={(value) => (inputValueRef.current.value = value)}
         {...rest}
       />
     </S.Container>
